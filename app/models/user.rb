7be706b045f088e4d88201b060e9927c638919c6
australhawk <austral@gamerdeal.tv>
2013-06-05 14:47:35 -0400

    Added Gravatar url

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,15 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :profile_name
   # attr_accessible :title, :body
+
+  def full_name
+  	first_name + " " + last_name
+  end
+  def gravatar_url
+  	stripped_email = email.strip
+  	downcased_email = stripped_email.downcase
+  	hash = Digest::MD5.hexdigest(downcased_email)
+
+  	"http://gravatar.com/avatar/#{hash}"
+  end
 end
