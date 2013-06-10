dcf89f5b7d482b208d803a6caf5057277e458fb3
australhawk <austral@gamerdeal.tv>
2013-06-10 10:21:57 -0400

    Commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,11 @@ class ApplicationController < ActionController::Base
 
   def initialize_cart
     if session[:cart_id]
-      
-      	@cart = Cart.find(session[:cart_id])
+    	@cart = Cart.find(session[:cart_id])
      
     else
-      @cart = Cart.create
-      session[:cart_id] = @cart.id
+      	@cart = Cart.create
+      	session[:cart_id] = @cart.id
     end
   end
 end
